f73945c818594db1aa086d7e81b253882524be1e
Sebastian Wolf <sebastian@mail-wolf.de>
2017-03-06 22:30:27 +0100

    made stream log possible

--- runserver.py
+++ runserver.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     app.logger.addHandler(fileHandler)
     app.logger.addHandler(streamHandler)
     app.logger.info("Logging is set up...")
-    app.run(HOST, PORT, debug=True)
+    app.run(HOST, PORT, debug=True, threaded=True)
