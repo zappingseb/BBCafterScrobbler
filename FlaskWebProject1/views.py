51ef71ce02fbbbb5ed543cc7e38c394a1d9dc787
Sebastian Wolf <sebastian@mail-wolf.de>
2017-03-06 22:14:44 +0100

    added the right API address

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -268,7 +268,7 @@ def home():
                        "&api_sig=",
                        doc["api_secret"]
                        ])
-
+        print("YEH")
         s = requests.Session()
 
         retries = Retry(total=1,
