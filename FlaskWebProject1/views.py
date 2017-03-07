c7d47cf76acb7671880009f2b96efedf192459df
zappingseb <sebastian@mail-wolf.de>
2017-03-07 23:41:25 +0100

    Made it possible to work with JSON of BBC

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -1,20 +1,42 @@
 """
-Routes and views for the flask application.
+Flask App for BBC After Scrobbler
+
+author: Sebastian Wolf
+description: This is the view file generating the substantial
+    view from the index.html File
+
+Steps of the workflow:
+
+    * Check if the user is logged in
+    * Login the user via last.fm and put session_key into session
+    * Check if a songlist was sent
+    ** Scrobble songs from list
+    * Check if an episode was sent
+    ** Get the songs of this episode and display them
+    * Check if a BBC Radio station and time were sent
+    ** Generate a list of episodes to be displayed in a form
 """
+__author__ = "Sebastian Wolf"
+__copyright__ = "Copyright 2017, BBCAfterSCrobbler"
+__license__ = "https://www.apache.org/licenses/LICENSE-2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Sebastian Wolf"
+__email__ = "sebastian@mail-wolf.de"
+__status__ = "Production"
+
 
 from datetime import datetime
 from flask import render_template, request, session
 from FlaskWebProject1 import app
 import os
 import json
-
 from flask_wtf import FlaskForm
-from wtforms import DateField, SelectField, validators, ValidationError
+from wtforms import DateField, SelectField, ValidationError
 import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter, ConnectionError
 import pylast
-from pylast import NetworkError, WSError
+from pylast import NetworkError
 import yaml
 
 def get_secret_dict(secrets_file="FlaskWebProject1/static/content/test_pylast.yaml"):
@@ -26,6 +48,15 @@ def get_secret_dict(secrets_file="FlaskWebProject1/static/content/test_pylast.ya
     return doc
 
 def validate_it(form, chain):
+    """ Function to validate if no future date was sent
+
+    :param form: Any Flaskform from the app
+    :type form: FlaskForm
+
+    :param chain: Data of the flaskform in a chained format
+
+    :return: Raise Validationerror if date is in the future
+    """
 
     input_time = datetime.strptime(chain.raw_data[0], chain.format)
 
@@ -34,11 +65,26 @@ def validate_it(form, chain):
 
 
 class DateForm(FlaskForm):
+    """ Generate a dateform in american date format
+
+    This generates a date selecter that will be rendered
+    by Jquery UI as a DatePicker later on (see static/templates/index.html)
+
+    """
     dt = DateField('Pick a date', format="%m/%d/%Y", validators=[validate_it])
 
 
 class BBCStationList(FlaskForm):
-    # https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/BBC.pm
+    """BBC Station List Dropdown Form
+
+    This generates a select field were a single BBC Station
+    can be selected by the user
+
+    Names of BBC stations can be found at:
+
+        https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/BBC.pm
+    """
+
     radiostation = SelectField(
         'Radio Station',
         choices=[("radio1", "BBC Radio 1 London"),
@@ -50,19 +96,54 @@ class BBCStationList(FlaskForm):
 
 
 class GetterOfIt(object):
+    """Class to access BBC Stations API
+
+    """
     def get_bbc_json(self, datestring, radiostation_name):
+        """Accessing a JSON file provided by the BBC API as
+        described at
 
-        print("WENT TO BBC FUNCTION")
+        http://www.bbc.co.uk/blogs/radiolabs/2008/05/helping_machines_play_with_pro.shtml
 
-        s = requests.Session()
+        :param datestring: A string providing a date in "%m/%d/%y" format
 
+        :type datestring: str
+
+        :param radiostation_name: A BBC Radio Station name in short format
+            as it can be used by the API
+
+        :return: imagelist, hidden_content
+
+            imagelist: A List of episodes pid, title and subtitle
+                Each episode is a dictionary with these fields
+
+            hidden_content: A string of the list of data which contains dictionaries with the following fields
+                per dictionary:
+
+                * title = Name of the episode
+                * subtitle = Subtitle of the episode
+                * start = Starting time
+                * end = Ending time
+                * radio = radio station name
+        """
+
+        # Parse the datestring
         datestring_new = datetime.strptime(datestring, "%m/%d/%y").strftime('%Y/%m/%d')
 
+        # start requests session
+        s = requests.Session()
+
+
+        # Define a Retry Element to have the possibility to
         retries = Retry(total=1,
                         backoff_factor=0.1,
                         status_forcelist=[ 500, 502, 503, 504])
 
         s.mount('http://', HTTPAdapter(max_retries=retries))
+
+
+        # Get the json from the specific radiostation by using the
+        # BBC API
         if radiostation_name=="radio4":
             try:
                 request = s.get("".join([
@@ -101,38 +182,55 @@ class GetterOfIt(object):
             except ConnectionError:
                  my_json = json.load(open("FlaskWebProject1/static/tests/yesterday.json"))
 
-        print("GOT JSON")
-
+        # Parse a short list of station information
         imagelist = self._receive_images(my_json)
+
+        # Get a large list of the stations information
 
         hidden_content = self._receive_hidden(my_json)
 
+        # Return both lists as strings (json.dumps)
         return imagelist, hidden_content
 
-    def _receive_images(self,my_json):
+    def _receive_images(self, my_json):
+        """ Derive a short dictionary with BBC Episode information
+
+        :param my_json: Dictionary of the JSON Provided by the BBC Station API
+
+        :return: imagelist
+
+            A List of episodes pid, title and subtitle
+
+            Each episode is a dictionary with these fields
+        """
 
         my_images=list()
         for episode in my_json["schedule"]["day"]["broadcasts"]:
-            print(episode["programme"]["display_titles"]["title"])
-            print("".join(["sub:",episode["programme"]["display_titles"]["subtitle"]]))
-            # my_images.append(
-            #    "".join(["<image pid=\"",
-            #             episode["programme"]["image"]["pid"],
-            #             "\" template_url=\"http://ichef.bbci.co.uk/images/ic/16x16/",
-            #             episode["programme"]["image"]["pid"],
-            #             ".jpg\" />"])
-            # )
+
             my_images.append(
                 [episode["programme"]["image"]["pid"],
                  episode["programme"]["display_titles"]["title"],
                  episode["programme"]["display_titles"]["subtitle"]
                  ])
 
-        print(my_images.__len__())
-
         return my_images
 
-    def _receive_hidden(self,my_json):
+    def _receive_hidden(self, my_json):
+        """ Read out essential data for each episode in a BBC JSON
+
+        :param my_json: Dictionary containing a JSON from the BBC API
+
+        :return: json.dumps(list_of_data)
+
+            A string of the list of data which contains dictionaries with the following fields
+            per dictionary:
+
+                * title = Name of the episode
+                * subtitle = Subtitle of the episode
+                * start = Starting time
+                * end = Ending time
+                * radio = radio station name
+        """
         list_of_data = list()
         for episode in my_json["schedule"]["day"]["broadcasts"]:
             list_of_data.append(
@@ -148,6 +246,13 @@ class GetterOfIt(object):
 
 
 class LastFMDataGetter():
+    """ A class to derive basic connectivities with
+    the last.fm API
+
+    On init it connects to the last.fm API with a session key
+
+    Without a session key it will cause a WSError
+    """
 
     def __init__(self):
         doc = get_secret_dict()
@@ -160,10 +265,38 @@ class LastFMDataGetter():
     def derive_track_dict(self,
                           jsonstring,
                           index):
+        """Derive a list of tracks played by a specific BBC radio station
+        in a specific time range
+
+        Tasks:
+            * Get the episode information
+            * Convert the timestrings
+            * Convert the radio station name to last.fm user account name
+            * Derive the tracks by using pylast.get_recent_tracks
+
+        :param jsonstring: A dictionary of BBC radio station episode information
+            containing dictinaries. It is normally produced by
+            :func:`GetterOfIt._receive_hidden`
+
+        :param index: The number of the element of the list in the jsonstring that
+            shall be used for calling the API
+
+        :type index: int
+
+        :return: tracks
+
+            A list of last.fm PlayedTracks (pylast.Track) elements
+            that were played in the time range of the defined
+            episode
+
+        :rtype: list
+        """
+
         data_list = json.loads(jsonstring)
         data_dict = data_list[int(index)]
 
-        print(data_dict["radio"])
+        # Rename the radio station to BBC user Names
+
         if data_dict["radio"]=="bbcone":
             radio = "BBCRadio1"
         elif data_dict["radio"]=="radio3":
@@ -171,7 +304,7 @@ class LastFMDataGetter():
         else:
             radio = "".join(["bbc",data_dict["radio"]])
 
-        lastfm_user = self.network.get_user(radio)
+        # Convert the datestrings of start and end of the episode to UTC
 
         start = datetime.strptime(data_dict["start"], "%Y-%m-%dT%H:%M:%SZ")
         end = datetime.strptime(data_dict["end"], "%Y-%m-%dT%H:%M:%SZ")
@@ -179,13 +312,30 @@ class LastFMDataGetter():
         utc_start = calendar.timegm(start.utctimetuple())
         utc_end = calendar.timegm(end.utctimetuple())
 
-        # Act
+        # Derive the tracks from pylast
+        lastfm_user = self.network.get_user(radio)
         tracks = lastfm_user.get_recent_tracks(time_from=utc_start,
                                                time_to=utc_end,limit=1000)
 
         return tracks
 
     def derive_episode_text(self,jsonstring, index):
+        """ Derive a string for the name of the episode plus the time
+        it was played at to be displayed in the app
+
+        :param jsonstring: A dictionary of BBC radio station episode information
+            containing dictinaries. It is normally produced by
+            :func:`GetterOfIt._receive_hidden`
+
+        :param index: The number of the element of the list in the jsonstring that
+            shall be used for calling the API
+
+        :type index: int
+
+        :return: An information string for the episode
+
+            Like "Jamie Cullum - Friday Night show - Friday, March 20 2016 20:00"
+        """
         data_list = json.loads(jsonstring)
         data_dict = data_list[int(index)]
         return " ".join([data_dict["title"]," - ",
@@ -195,6 +345,36 @@ class LastFMDataGetter():
                                '%A, %d %b %Y %H:%M'))])
 
     def derive_form_data(self, hiddenjson, index):
+        """ From basic BBC Episode information + an index of the episode
+        derive the songs played in two formats and the name+Playtime
+
+        Tasks:
+
+            * Derive a list of tracks of the episode by pylast
+            * In case the list is not empty generate a list with
+            ** title
+            ** artist
+            ** timestamp
+            * and another list with
+            ** counter(i), "artist - title"
+
+            The first list shall go in the hidden content of a flaskform. The second list
+            is displayed to the user to select songs
+
+            * Derive the name of the episode which songs are displayed
+
+        :param hiddenjson: A dictionary of BBC radio station episode information
+            containing dictinaries. It is normally produced by
+            :func:`GetterOfIt._receive_hidden`
+
+        :param index: The number of the element of the list in the jsonstring that
+            shall be used for calling the API
+
+        :type index: int
+
+        :return: song_tuples, songlist_form_hidden_data, episode_string
+
+        """
         tracklist = self.derive_track_dict(hiddenjson, index)
 
         try:
@@ -212,6 +392,7 @@ class LastFMDataGetter():
             else:
                 song_tuples=[]
                 songlist_form_hidden_data=""
+
         except NetworkError:
             song_tuples = [(1,"testsong"),
                                (2,"testsong2")]
@@ -222,31 +403,63 @@ class LastFMDataGetter():
                self.derive_episode_text(hiddenjson,index)
 
     def scrobble_from_json(self, jsonstring="", indeces=list()):
+        """From a json of Songs and a list of indeces scrobble songs to the last.fm API
 
+        This uses pylast.scrobble_many to simply scrobbe a list of songs from a jsonstring
+        that contains these songs and a list of indeces which songs to take from that list
+
+        :param jsonstring: A json put into a string. the json was compiled by :func:`songlist_form_hidden_data
+
+        :param indeces: A list of integers telling which elements to take from the songlist and scrobble them
+
+        :return: The list of songs as "Artist - Title - Timestamp" to be displayed in the app
+        """
         data_list = json.loads(jsonstring)
 
         tracklist = [{"title":data_list[index]["title"],
                       "artist":data_list[index]["artist"],
                       "timestamp":data_list[index]["timestamp"]}
                      for index in indeces]
-        print("in_scrobble")
-        self.network.scrobble_many(tracks=tracklist)
+        try:
+            self.network.scrobble_many(tracks=tracklist)
 
-        scrobbling_list = [" - ".join([
-            data_list[index]["artist"],
-            data_list[index]["title"],
-            datetime.fromtimestamp(int(
-                data_list[index]["timestamp"])
-            ).strftime('%Y-%m-%d %H:%M')
-        ]) for index in indeces]
+            scrobbling_list = [" - ".join([
+                data_list[index]["artist"],
+                data_list[index]["title"],
+                datetime.fromtimestamp(int(
+                    data_list[index]["timestamp"])
+                ).strftime('%Y-%m-%d %H:%M')
+            ]) for index in indeces]
+        except:
+            scrobbling_list = False
 
         return scrobbling_list
 
 class LoginCheck(object):
-    def check(self,the_session_key, lastfm_token):
-        doc = get_secret_dict()
-        if the_session_key is not None:
+    """ Class to perform Login at the last.fm API
 
+    This class just carries one function that checks if a login
+    has to be performed or already was performed.
+
+    """
+    def check(self,the_session_key, lastfm_token):
+        """ Depending on the existence of a session key or lastfm_token
+        tell the app if the user is logged in or not
+
+        :param the_session_key: A session key derived by pylast.SessionKeyGenerator
+
+        :param lastfm_token: A token derived by calling the last.fm API as
+            on http://www.last.fm/api/webauth
+
+        :return: True (user is logged in) | False (user not logged in)
+        """
+
+        # Derive secret API key and API secret form yaml file
+        doc = get_secret_dict()
+
+        # If a session_key is there
+        if the_session_key is not None:
+            # try to connect at last.fm
             try:
                 network=pylast.LastFMNetwork(api_key=doc["api_key"],
                                         api_secret=doc["api_secret"],
@@ -258,12 +471,14 @@ class LoginCheck(object):
 
             return logged_in
         else:
+            # If a token was provided
             if lastfm_token is not None:
 
                 network = pylast.LastFMNetwork(api_key=doc["api_key"],
                                             api_secret=doc["api_secret"],
                                             username=doc["username"])
 
+                # Create a session key by using pylast
                 sk_gen = pylast.SessionKeyGenerator(network)
                 session_key = sk_gen.get_web_auth_session_key(url=None,
                                                               token=lastfm_token)
@@ -280,6 +495,7 @@ class LoginCheck(object):
 @app.route('/home')
 def home():
 
+    # Generate a starting formula using WTF Forms
     form = DateForm()
     form2 = BBCStationList()
 
@@ -292,17 +508,22 @@ def home():
     the_session_key = session.get("session_key", None)
 
 
-
+    # Check if the user generated a login session key
     login_checker = LoginCheck()
 
     logged_in = login_checker.check(the_session_key, lastfm_token)
 
+    # In case the user is logged in already
     if logged_in:
 
+        # If a BBC episode was sent via POST
         if index_of_episode is not None:
+
+            # get the hidden data of the BBC episode
             hiddenjson = request.form.get('hiddentype', None)
             last_fm_getter = LastFMDataGetter()
 
+            # Derive the songs of the BBC episode
             song_tuples, songlist_form_hidden_data, songlist_episode = \
                 last_fm_getter.derive_form_data(hiddenjson,index_of_episode)
 
@@ -314,7 +535,8 @@ def home():
                     form=form,
                     form2=form2,
                     superstring="No Songs found on last.fm",
-                    logged_in=logged_in
+                    logged_in=logged_in,
+                    set_tab="songchoose"
                 )
             else:
                 """Renders the BBC last.fm - ."""
@@ -330,11 +552,19 @@ def home():
                     logged_in=logged_in,
                     set_tab="songchoose"
                 )
-
+        # Check if a list of songs was selected by
+        # the user and send via POST
         elif len(indeces_of_songs) > 0:
+            # convert indeces to integer
             indeces_of_songs = [int(song) for song in indeces_of_songs]
+
+            # Get the hiddenjson containing song infos
             hiddenjson = request.form.get('hiddensongs', None)
+
+
             if hiddenjson is not None:
+
+                # Connect to last.fm and scrobble songs
                 last_fm_getter = LastFMDataGetter()
                 scrobbling_list = last_fm_getter.scrobble_from_json(hiddenjson,
                                                                     indeces_of_songs)
@@ -359,6 +589,9 @@ def home():
                         superstring = "Problems scrobbling, click Refresh",
                         logged_in=logged_in
                     )
+
+        # Check if a date and BBC Radio station were selected and
+        # send via POST
         elif form.validate_on_submit():
 
             print("mygetter")
@@ -389,6 +622,7 @@ def home():
                 episodes=list(),
                 logged_in=logged_in
             )
+    # Not logged in
     else:
         """Renders the BBC last.fm - ."""
         return render_template(
@@ -400,23 +634,3 @@ def home():
             episodes=list(),
             superstring="Please login first"
         )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
--- runserver.py
+++ runserver.py
@@ -1,54 +1,24 @@
 """
-This script runs the FlaskWebProject1 application using a development server.
+Flask App for BBC After Scrobbler
+
+author: Sebastian Wolf
+description: This is a flask app giving the ability to scrobble
+    last.fm tracks from BBC Radio Shows
+
 """
+__author__ = "Sebastian Wolf"
+__copyright__ = "Copyright 2017, BBCAfterSCrobbler"
+__license__ = "https://www.apache.org/licenses/LICENSE-2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Sebastian Wolf"
+__email__ = "sebastian@mail-wolf.de"
+__status__ = "Production"
 
-
-import pip
-try:
- import six
-except:
- package = 'six-1.10.0-py2.py3-none-any.whl'
- pip.main(['install', '--user', package])
- raise ImportError('Restarting')
-try:
- import pylast
- from pylast import NetworkError, WSError
-except:
- package = 'pylast'
- pip.main(['install', '--user', package])
- raise ImportError('Restarting')
-try:
- from flask_wtf import FlaskForm
-except:
- package = 'Flask-WTF'
- pip.main(['install', '--user', package])
- raise ImportError('Restarting')
-try:
- from wtforms import DateField, SelectField, validators, ValidationError
-except:
- package = 'WTForms'
- pip.main(['install', '--user', package])
- raise ImportError('Restarting')
-
-try:
-  import requests
-  from requests.packages.urllib3.util.retry import Retry
-  from requests.adapters import HTTPAdapter, ConnectionError
-except:
- package = 'requests'
- pip.main(['install', '--user', package])
- raise ImportError('Restarting')
 from os import environ
 from FlaskWebProject1 import app
 import logging
 
 if __name__ == '__main__':
-    try:
-     from flask_wtf import FlaskForm
-    except:
-     package = 'Flask-WTF'
-     pip.main(['install', '--user', package])
-     raise ImportError('Restarting')
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
